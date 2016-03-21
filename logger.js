88b2e5a569631f0cfe55cb68908bd3f71ef9cd4f
somdoron <somdoron@gmail.com>
2016-03-21 15:46:32 +0200

    logging to papertrail

--- logger.js
+++ logger.js
@@ -1,18 +1,36 @@
-var winston = require('winston')
+const winston = require('winston')
+const process = require('process')
+const Papertrail = require('winston-papertrail').Papertrail
 
-var logger = new winston.Logger({
-    transports: [
-        new (winston.transports.Console)({
-            handleExceptions: true,
-            humanReadableUnhandledException: true
-        }),
-        new (winston.transports.File)({ filename: 'betaprod.log' })
-    ],
-    exceptionHandlers: [
-        new winston.transports.File({ filename: 'exceptions.log',
-            humanReadableUnhandledException: true
-        })
-    ]
-})
-
-module.exports = logger
+if (process.env.NODE_ENV == 'production') {
+    module.exports = new winston.Logger({
+        transports: [
+            new Papertrail({
+                host: process.env.PPRTRL_HOST,
+                port: process.env.PPRTRL_PORT,
+                handleExceptions: true
+            }),
+            new (winston.transports.Console)({
+                handleExceptions: true,
+                humanReadableUnhandledException: true
+            })
+        ]
+    })
+}
+else {
+    module.exports = new winston.Logger({
+        transports: [
+            new (winston.transports.Console)({
+                handleExceptions: true,
+                humanReadableUnhandledException: true
+            }),
+            new (winston.transports.File)({filename: 'betaprod.log'})
+        ],
+        exceptionHandlers: [
+            new winston.transports.File({
+                filename: 'exceptions.log',
+                humanReadableUnhandledException: true
+            })
+        ]
+    })
+}
