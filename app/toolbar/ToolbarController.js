4e5f38fe6c4f073b26838bcd3d677d0558cf6730
Oren Reuveni <orenreu@gmail.com>
2016-03-24 18:05:06 +0200

    Add musl log in to post product

--- app/toolbar/ToolbarController.js
+++ app/toolbar/ToolbarController.js
@@ -7,12 +7,18 @@
  */
 
 
-function ToolbarController($scope, $http, $mdDialog, $mdMedia) {
+function ToolbarController($scope, $http, $mdDialog, $mdMedia, $state) {
 
     //EDIT Project
     $scope.login = function(){
         openLogintDialog();
     }
+
+    $scope.user = [];
+
+    $http.get("/api/user").then(response => {
+        $scope.user = response.data
+    })
 
 
     //Open the Edit Project Dialog
@@ -39,8 +45,29 @@ function ToolbarController($scope, $http, $mdDialog, $mdMedia) {
         }
     };
 
+
+    $scope.postProduct = function(){
+
+        if(isLogged()){
+            $state.go("post");
+        } else {
+            openLogintDialog()
+        }
+
+
+    }
+
+    function isLogged(){
+        if( $scope.user.id) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
 }
 
 module.exports = function(app) {
-    app.controller('ToolbarController', ['$scope', '$http', '$mdDialog', '$mdMedia', ToolbarController])
+    app.controller('ToolbarController', ['$scope', '$http', '$mdDialog', '$mdMedia', '$state', ToolbarController])
 }
