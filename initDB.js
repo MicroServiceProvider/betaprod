40d3cc68fa8d5975251be546a482cc87b2ce7f2c
somdoron <somdoron@gmail.com>
2016-03-24 11:17:56 +0200

    delete all db before initialize db

--- initDB.js
+++ initDB.js
@@ -1,14 +1,18 @@
-var User = require('./models').User
-var Product = require('./models').Product
+const r = require('./models').r
+const User = require('./models').User
+const Product = require('./models').Product
+const process = require('process')
 
-User.save({
-    firstName:'Doron',
-    lastName: 'Somech',
-    email:'doron.somech@gmail.com',
-    provider:'facebook',
-    providerId: '10153767634642935'
-}).then(user=> {
-    console.log('user saved')
+r.db('betaprod').table('User').delete().run().
+then(() => r.db('betaprod').table('Product').delete().run()).
+then(() => User.save({
+        firstName:'Doron',
+        lastName: 'Somech',
+        email:'doron.somech@gmail.com',
+        provider:'facebook',
+        providerId: '10153767634642935'
+    })).
+then(user =>
     Product.save({
         userId: user.id,
         name:'betaprod',
@@ -23,8 +27,21 @@ User.save({
         'will uncover many web sites still in their infancy. ' +
         'Various versions have evolved over the years, sometimes by accident, ' +
         ' on purpose (injected humour and the like).',
-        imageUrl:'https://unsplash.com/photos/TMOeGZw9NY4'
-    }).then(() =>{
-        console.log('product saved')
-    })
+        imageUrl:'https://unsplash.com/photos/TMOeGZw9NY4',
+        description:'Lorem ipsum dolor sit amet, duo nullam labore albucius ut, ' +
+        'suscipit voluptua te nec. Eum no invenire interesset consectetuer, ' +
+        'no sed illud commune accusata. Tacimates suavitate has at, option indoctum ' +
+        'vituperatoribus eam cu. Et quo quando perfecto scriptorem, mutat choro utinam' +
+        ' vis ex. Vivendo omittam ad per. Aliquid argumentum vix id, nihil alterum ' +
+        'consequuntur id eum.' +
+        'Ut esse mazim nihil pri, ut causae alterum has.' +
+        'Nam veniam theophrastus ne, id nec soleat alterum. His ' +
+        'solum facilisis quaerendum ei, eos ei tollit suscipiantur.' +
+        ' Cum prima aliquip iracundia ex. Quo ea ullum timeam sapientem.' +
+        ' Justo iudicabit vituperatoribus id eam, odio congue minimum te quo.',
+        website:'http://betaprod.co'
+    })).
+then(() => {
+    console.log('all done')
+    process.exit()
 })
