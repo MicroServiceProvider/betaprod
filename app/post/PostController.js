914a50fd8e1a119c569e3ffcece3f19fb54994b6
somdoron <somdoron@gmail.com>
2016-03-24 12:40:05 +0200

    post product'

--- app/post/PostController.js
+++ app/post/PostController.js
@@ -1,7 +1,15 @@
-function PostController($scope) {
-    $scope.message = 'hello posts'
+function PostController($scope, $http, $state) {
+    $scope.product = {}
+
+    $scope.submit = function () {
+        $http.post('/api/product', $scope.product).
+            then(p => {
+                console.log(p)
+                $state.go('product', {id: p.data.id})
+            })
+    }
 }
 
 module.exports = function(app) {
-    app.controller('PostController', ['$scope', PostController])
+    app.controller('PostController', ['$scope','$http', '$state', PostController])
 }
